7f3350d84b30c273a638cd9722494da0425e1d35
birb <parabirb@protonmail.ch>
2024-06-19 18:05:16 -0500

    PLEAAAAAAAASE

--- svelte.config.js
+++ svelte.config.js
@@ -9,7 +9,7 @@ const config = {
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(),
 		paths: {
-			assets: "https://parabirb.github.io/transphobe-search"
+			assets: "https://raw.githubusercontent.com/parabirb/transphobe-search/gh-pages"
 		}
 	},
 	preprocess: vitePreprocess()
